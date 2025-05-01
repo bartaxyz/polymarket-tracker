5e90dd5cc193fba95309b0b51669801e8a42f22e
Ondřej Bárta <email@ondrejbarta.xyz>
2025-05-01 19:50:16 +0200

    feat: add portfolio widget

---
+++ Portfolio/PortfolioBundle.swift
@@ -0,0 +1,16 @@
+//
+//  PortfolioBundle.swift
+//  Portfolio
+//
+//  Created by Ondřej Bárta on 1/5/25.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct PortfolioBundle: WidgetBundle {
+    var body: some Widget {
+        Portfolio()
+    }
+}
